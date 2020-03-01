b3d62c952651d8e88863fe21b36e12a02e96aedc
Pap Bence <43560591+hunter4961@users.noreply.github.com>
2020-03-01 20:29:25 +0100

    Add files via upload

---
+++ xorcsere.c
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main() {
+	
+	double a;
+	double b;
+	cout << "Adj meg egy számot: ";
+	cin >> a;
+	cout << "Adj meg egy másik számot: ";
+	cin >> b;
+
+	cout << "Megadott számok: " << a << " és " << b << '\n';
+
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+
+	ccout << "Felcserélés után: " << a << " és " << b << '\n'; 
+}
