cf0a30ea30fe2e6d5b39f40035631d27b64460f2
Pap Bence <43560591+hunter4961@users.noreply.github.com>
2020-04-27 22:56:08 +0200

    Update xorcsere.c

--- xorcsere.c
+++ xorcsere.c
@@ -1,19 +1,17 @@
-#include "std_lib_facilities.h"
+#include <iostream>
 
 int main() {
 	
 	double a;
 	double b;
-	cout << "Adj meg egy számot: ";
-	cin >> a;
-	cout << "Adj meg egy másik számot: ";
-	cin >> b;
-
-	cout << "Megadott számok: " << a << " és " << b << '\n';
+	std::cout << "Adj meg egy számot: ";
+	std::cin >> a;
+	std::cout << "Adj meg egy másik számot: ";
+	std::cin >> b;
 
 	a = a ^ b;
 	b = a ^ b;
 	a = a ^ b;
 
-	ccout << "Felcserélés után: " << a << " és " << b << '\n'; 
+	std::ccout << "Felcserélés után: " << a << " és " << b << '\n'; 
 }
